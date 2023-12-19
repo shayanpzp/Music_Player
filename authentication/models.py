716b91785f02f1122d320b279f6795cae595b68f
NIT Computer <user01@yahoo.com>
2023-12-19 10:29:17 +0330

    chore : update CustomUser and settings

--- authentication/models.py
+++ authentication/models.py
@@ -1,6 +1,20 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 class CustomUser(AbstractUser):
     is_premium = models.BooleanField(default=False)
-    # Add other fields if necessary
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name="customuser_groups",
+        blank=True,
+        help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.",
+        verbose_name="groups",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="customuser_permissions",
+        blank=True,
+        help_text="Specific permissions for this user.",
+        verbose_name="user permissions",
+    )
